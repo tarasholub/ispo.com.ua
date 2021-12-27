3842ca5ec1a5c2d5a0cc7211a0bb7b3f4a865707
tarasholub <holub.taras@gmail.com>
2021-12-27 02:44:44 +0200

    Updated language switcher

--- components/LanguageSwitcher.js
+++ components/LanguageSwitcher.js
@@ -3,10 +3,16 @@ import { useRouter } from 'next/router';
 
 const LanguageSwitcher = () => {
   const router = useRouter();
+  const pathname = router.pathname;
+  const uid = router.query.uid;
+  const path = pathname.replace('[uid]', uid);
+
   return (
     <ul>
       {router?.locales.map(lang => (
-        <li key={lang}>{lang}</li>
+        <li key={lang}>
+          <Link href={`${path}`} locale={lang}>{lang}</Link>
+        </li>
       ))}
     </ul>
   )
