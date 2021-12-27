c8efeea12a66e35da787f71b3009d6b495fcc601
tarasholub <holub.taras@gmail.com>
2021-12-27 02:46:19 +0200

    Fixed style

--- styles/header.styled.js
+++ styles/header.styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const HeaderWrapper = styled.header`
   display: flex;
+  flex-direction: row;
   justify-content: space-between;
   padding: 0 16px 32px;
   margin: 0 0 32px 0;
